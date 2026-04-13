8a2cdd89d78a6de712ac509b5c9e918fc6a1bfe6
NicoleMCosta <nicolemartinsdacosta@gmail.com>
2026-04-13 15:17:41 -0300

    template matching finalizado e video teste

--- template_matching/templateMatch.py
+++ template_matching/templateMatch.py
@@ -1,28 +1,65 @@
 import csv
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def rgb_to_gray(image):
-    #opencv reads BGR
-    r = image[:, :, 2]
-    g = image[:, :, 1]
-    b = image[:, :, 0]
-
-    gray = 0.3*r + 0.59*g + 0.11*b
-    return gray
-
-def csv_create(method):
-    with open(f'{method}_results.csv', mode='w', newline='') as file:
+def csv_create(method,i, min_val, max_val):
+    with open(f'{method}_results.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Frame', 'Min', 'Max'])
+        writer.writerow([i,min_val, max_val])
 
-    
+def method_apply():
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+    template = cv2.imread('./frames/template.png', 0)
+    w, h = template.shape[::-1]
+
+    for m in methods:
+        output_folder = f'./results_{m}'
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        for i in os.listdir('./frames'):
+            img = cv2.imread(f'./frames/{i}', 0)
+
+            if i != 'template.png':
+                img2 = img.copy()
+                method = eval(m)
+
+                result = cv2.matchTemplate(img2, template, method)
+                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                
+                # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+                if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+                    top_left = min_loc
+                else:
+                    top_left = max_loc
+                bottom_right = (top_left[0] + w, top_left[1] + h)
+
+                #criando e populando o csv de cada método
+                csv_create(m,i, min_val, max_val)
+
+                cv2.rectangle(img2,top_left, bottom_right, 255, 2)
+                plt.subplot(121)
+                plt.imshow(result,cmap = 'gray')
+                plt.title('Matching Result')
+                plt.xticks([])
+                plt.yticks([])
+
+                plt.subplot(122)
+                plt.imshow(img2,cmap = 'gray')
+                plt.title('Detected Point')
+                plt.xticks([])
+                plt.yticks([])
+                plt.suptitle(m)
+                
+                save_path = f'{output_folder}/res_{i}'
+                plt.savefig(save_path)
+                plt.close('all')
+                    
+
 def execution():
-    if not os.path.exists('./grayframes'):
-        os.makedirs('./grayframes')
+    
+    method_apply()
 
-    for i in os.listdir('./frames'):
-            img = cv2.imread(f'./frames/{i}')
-            gray_img = rgb_to_gray(img)
-            cv2.imwrite(f'./grayframes/gray_{i}', gray_img)
+execution()
